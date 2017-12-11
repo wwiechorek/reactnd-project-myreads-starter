ff62c24091f6169fc3fc7216796fc1053ed550ee
William Wiechorek <wwiechorek@MacBook-Air-de-William.local>
2017-12-11 08:29:56 -0200

    Fix: Removed state app.js - unnecessary

--- src/App.js
+++ src/App.js
@@ -7,22 +7,6 @@ import Search from './pages/Search'
 import Bookcase from './pages/Bookcase'
 
 class BooksApp extends React.Component {
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
-  }
-
-  searchPage(status) {
-    this.setState({
-      showSearchPage: status
-    })
-  }
-
   render() {
     return (
       <div className="app">
